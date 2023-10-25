1952e459c8c5578595dc0560aaec942ec34d0aec
Mythos-404 <mythos_404@outlook.com>
2023-10-25 17:31:46 +0800

    fix(plugin): remove `sections`

--- lua/modules/configs/tool/lsp-lens.lua
+++ lua/modules/configs/tool/lsp-lens.lua
@@ -2,11 +2,6 @@ return function()
 	require("lsp-lens").setup({
 		enable = true,
 		include_declaration = false, -- Reference include declaration
-		sections = { -- Enable / Disable specific request
-			definition = false,
-			references = true,
-			implements = true,
-		},
 		ignore_filetype = {
 			"prisma",
 		},
