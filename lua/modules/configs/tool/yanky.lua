58ea258e690b09c5a4b5c5e51a7ab76484ad705d
Mythos-404 <mythos_404@outlook.com>
2023-12-15 23:59:08 +0800

    feat(yanly): configure copy and paste highlighting

--- lua/modules/configs/tool/yanky.lua
+++ lua/modules/configs/tool/yanky.lua
@@ -1,5 +1,13 @@
 return function()
 	require("yanky").setup({
+		preserve_cursor_position = {
+			enabled = true,
+		},
+		highlight = {
+			on_put = true,
+			on_yank = true,
+			timer = 500,
+		},
 		ring = {
 			storage = "sqlite",
 		},
