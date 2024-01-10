e5db6edba0fa1e09191bea0f73d181d2a8be6a3c
Mythos-404 <mythos_404@outlook.com>
2024-01-11 00:38:18 +0800

    perf(plugin): remove `neodim` and `specs.nvim`

--- lua/modules/plugins/ui.lua
+++ lua/modules/plugins/ui.lua
@@ -36,11 +36,11 @@ ui["nvim-lualine/lualine.nvim"] = {
 	event = { "BufReadPost", "BufAdd", "BufNewFile" },
 	config = require("ui.lualine"),
 }
-ui["zbirenbaum/neodim"] = {
-	lazy = true,
-	event = "LspAttach",
-	config = require("ui.neodim"),
-}
+-- ui["zbirenbaum/neodim"] = {
+-- 	lazy = true,
+-- 	event = "LspAttach",
+-- 	config = require("ui.neodim"),
+-- }
 if vim.g.neovide or false then
 	ui["karb94/neoscroll.nvim"] = {
 		lazy = true,
@@ -69,10 +69,10 @@ ui["dstein64/nvim-scrollview"] = {
 	event = { "BufReadPost", "BufAdd", "BufNewFile" },
 	config = require("ui.scrollview"),
 }
-ui["edluffy/specs.nvim"] = {
-	lazy = true,
-	event = "CursorMoved",
-	config = require("ui.specs"),
-}
+-- ui["edluffy/specs.nvim"] = {
+-- 	lazy = true,
+-- 	event = "CursorMoved",
+-- 	config = require("ui.specs"),
+-- }
 
 return ui
