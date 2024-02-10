85001c1daefe4a2b554516014e5bfd7c95679746
Mythos_404 <mythos_404@outlook.com>
2024-02-11 04:25:59 +0800

    feat(treesitter): add hyprlang TS

--- lua/modules/plugins/editor.lua
+++ lua/modules/plugins/editor.lua
@@ -153,6 +153,7 @@ editor["nvim-treesitter/nvim-treesitter"] = {
 			"JoosepAlviste/nvim-ts-context-commentstring",
 			config = require("editor.ts-context-commentstring"),
 		},
+		{ "luckasRanarison/tree-sitter-hyprlang" },
 		{ "IndianBoy42/tree-sitter-just", config = true },
 	},
 }
