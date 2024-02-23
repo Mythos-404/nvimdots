07c8a8d962cdf8f9a6bc04e95594508b4c6a3fd2
Mythos_404 <mythos_404@outlook.com>
2024-02-23 23:44:18 +0800

    fix(formatters): `clang_format` remove `.clang_format` file to command args

--- lua/modules/configs/completion/formatters/clang_format.lua
+++ lua/modules/configs/completion/formatters/clang_format.lua
@@ -1,1 +1,3 @@
-return { "-style=file" }
+return {
+	"-style={BasedOnStyle: LLVM, IndentWidth: 4, AlignArrayOfStructures: Left, AlignConsecutiveAssignments: { Enabled: true, AlignCompound: true }, AlignConsecutiveBitFields: { Enabled: true, AlignCompound: true, PadOperators: true }, AlignConsecutiveDeclarations: { Enabled: true, AlignCompound: true, PadOperators: true }, AlignConsecutiveMacros: { Enabled: true, AlignCompound: true, PadOperators: true }, BraceWrapping: { AfterClass: true }, IncludeBlocks: Regroup}",
+}
