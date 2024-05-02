cc2d28bdcbfef0e6f2a7dfe6565e329032a24f4a
Mythos_404 <mythos_404@outlook.com>
2024-05-02 18:43:04 +0800

    fix(formatters): always adding a trailing comma causes json-lsp to keep warning

--- lua/modules/configs/completion/formatters/prettier.lua
+++ lua/modules/configs/completion/formatters/prettier.lua
@@ -1,1 +1,1 @@
-return { "--tab-width", vim.api.nvim_get_option_value("shiftwidth", { scope = "local" }) }
+return { "--tab-width", vim.api.nvim_get_option_value("shiftwidth", { scope = "local" }), "--trailing-comma", "none" }
