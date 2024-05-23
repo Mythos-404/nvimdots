d6da1ad4a27b810142e0a893c1e84fdc13b6016c
Mythos_404 <mythos_404@outlook.com>
2024-05-23 10:14:50 +0800

    feat!(cmp): set comparators and sourcs

--- lua/modules/configs/completion/cmp.lua
+++ lua/modules/configs/completion/cmp.lua
@@ -47,16 +47,25 @@ return function()
 		sorting = {
 			priority_weight = 2,
 			comparators = {
-				compare.offset, -- Items closer to cursor will have lower priority
+				compare.offset,
 				compare.exact,
-				-- compare.scopes,
-				compare.lsp_scores,
-				compare.sort_text,
 				compare.score,
-				compare.recently_used,
-				-- compare.locality, -- Items closer to cursor will have higher priority, conflicts with `offset`
-				require("cmp-under-comparator").under,
+
+				function(entry1, entry2)
+					local _, entry1_under = entry1.completion_item.label:find("^_+")
+					local _, entry2_under = entry2.completion_item.label:find("^_+")
+					entry1_under = entry1_under or 0
+					entry2_under = entry2_under or 0
+					if entry1_under > entry2_under then
+						return false
+					elseif entry1_under < entry2_under then
+						return true
+					end
+				end,
+
+				compare.lsp_scores,
 				compare.kind,
+				compare.sort_text,
 				compare.length,
 				compare.order,
 			},
@@ -141,12 +150,10 @@ return function()
 			{ name = "nvim_lsp", priority = 100, max_item_count = 350 },
 			{ name = "nvim_lua", priority = 100 },
 			{ name = "luasnip", priority = 90 },
-			{ name = "natdat" },
 			{ name = "path" },
 			{ name = "treesitter" },
-			{ name = "spell" },
-			{ name = "tmux" },
-			{ name = "orgmode" },
+			-- { name = "spell" },
+			-- { name = "tmux" },
 			{
 				name = "buffer",
 				option = {
@@ -155,7 +162,7 @@ return function()
 					end,
 				},
 			},
-			{ name = "latex_symbols" },
+			-- { name = "latex_symbols" },
 		},
 		experimental = {
 			ghost_text = {
