02819e74e4992f5bc0685623224d0a7de0939315
Mythos_404 <mythos_404@outlook.com>
2024-05-23 10:11:48 +0800

    feat(plugin): set `nvim-ts-autotag` enable opts

--- lua/modules/configs/editor/autotag.lua
+++ lua/modules/configs/editor/autotag.lua
@@ -1,5 +1,10 @@
 return function()
 	require("nvim-ts-autotag").setup({
+		opts = {
+			enable_close = true, -- Auto close tags
+			enable_rename = true, -- Auto rename pairs of tags
+			enable_close_on_slash = true, -- Auto close on trailing </
+		},
 		filetypes = {
 			"html",
 			"javascript",
