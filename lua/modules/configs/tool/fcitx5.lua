9a88eafa7f928b3bf1ec62509a5158b4ff374bb2
Mythos_404 <mythos_404@outlook.com>
2024-06-05 12:56:11 +0800

    feat!(plugin): open `fcitx5.nvim`

---
+++ lua/modules/configs/tool/fcitx5.lua
@@ -0,0 +1,21 @@
+return function()
+	local en = "keyboard-us"
+	local zh_cn = "rime"
+	require("fcitx5").setup({
+		msg = nil, -- string | nil: printed when startup is completed
+		imname = { -- fcitx5.Imname | nil: imnames on each mode set as prior. See `:h map-table` for more in-depth information.
+			norm = en, -- string | nil: imname to set in normal mode. if nil, will restore the mode on exit.
+			ins = zh_cn,
+			cmd = en,
+			vis = en,
+			sel = en,
+			opr = en,
+			term = en,
+			lang = en,
+		},
+		remember_prior = false, -- boolean: if true, it remembers the mode on exit and restore it when entering the mode again.
+		--                                 if false, uses what was set in config.
+		define_autocmd = true, -- boolean: if true, defines autocmd at `ModeChanged` to switch fcitx5 mode.
+		log = "warn", -- string: log level (default: warn)
+	})
+end
