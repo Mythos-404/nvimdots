521196e568045788ab2690e7082d1a8ef4b0d120
Mythos_404 <mythos_404@outlook.com>
2024-07-01 23:39:24 +0800

    fix(cmp): lua_ls config determine nvim config dir

--- lua/modules/configs/completion/servers/lua_ls.lua
+++ lua/modules/configs/completion/servers/lua_ls.lua
@@ -1,7 +1,7 @@
 -- https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/server_configurations/lua_ls.lua
 return {
 	on_init = function(client)
-		if not (client.config.cmd_cwd:find("nvim") ~= 0) then
+		if not client.config.cmd_cwd:find("nvim") then
 			return
 		end
 
