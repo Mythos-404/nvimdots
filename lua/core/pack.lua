e788d3d448eaa4757afc296d320e92ad835e85a6
Mythos_404 <mythos_404@outlook.com>
2024-09-13 21:50:19 +0800

    feat(pack): disabled `editorconfig` plugin

--- lua/core/pack.lua
+++ lua/core/pack.lua
@@ -118,6 +118,12 @@ function Lazy:load_lazy()
 				---@type string[]
 				paths = {}, -- add any custom paths here that you want to include in the rtp
 				disabled_plugins = {
+					-- Set this to true in order to enable native EditorConfig support
+					-- WARN: Sleuth.vim already includes all the features provided by this plugin.
+					--       Do NOT enable both at the same time, or you risk breaking the entire detection system.
+					"editorconfig",
+					-- Do not load spell files
+					"spellfile",
 					-- Do not use builtin matchit.vim and matchparen.vim because we're using vim-matchup
 					"matchit",
 					"matchparen",
@@ -136,8 +142,6 @@ function Lazy:load_lazy()
 					--  > Disabling rplugin.vim will make `wilder.nvim` complain about missing rplugins during :checkhealth,
 					--  > but since it's config doesn't require python rtp (strictly), it's fine to ignore that for now.
 					"rplugin",
-					-- Do not load spell files
-					"spellfile",
 				},
 			},
 		},
