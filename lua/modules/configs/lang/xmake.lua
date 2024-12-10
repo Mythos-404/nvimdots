68b1efc0336a982344fde260ff1b3528caf07b2d
Mythos_404 <mythos_404@outlook.com>
2024-12-10 22:12:57 +0800

    feat(keymap): set `xmake.nvim` keymap

--- lua/modules/configs/lang/xmake.lua
+++ lua/modules/configs/lang/xmake.lua
@@ -1,7 +1,19 @@
+local bind = require("keymap.bind")
+local map_cmd = bind.map_cmd
+
+local xmake_keymap = {
+    ["n|<leader>xr"] = map_cmd(":Xmake run"):with_noremap():with_desc("xmake: Run target"),
+    ["n|<leader>xc"] = map_cmd(":Xmake clean"):with_noremap():with_desc("xmake: Clean target"),
+    ["n|<leader>xd"] = map_cmd(":Xmake debug"):with_noremap():with_desc("xmake: Debug target"),
+    ["n|<leader>xm"] = map_cmd(":Xmake mode"):with_noremap():with_desc("xmake: Switch buildmode"),
+}
+
 return function()
     require("xmake").setup({
         lsp = {
             language = "zh-cn",
         },
     })
+
+    bind.nvim_load_mapping(xmake_keymap)
 end
