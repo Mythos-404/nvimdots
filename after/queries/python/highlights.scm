d53d5a7319bf56e38ad6aafb2a5f715ad32dcb12
Mythos_404 <mythos_404@outlook.com>
2024-12-10 22:03:26 +0800

    format: set queries comment

--- after/queries/python/highlights.scm
+++ after/queries/python/highlights.scm
@@ -1,18 +1,18 @@
-;;extends
+;; extends
 
 ;; Math Operators
 ("lambda" @keyword.function (#set! conceal "λ"))
 
 ;; Important builtins
-(((attribute) @constant) (#eq? @constant "math\.pi") (#set! conceal "π"))
-(((attribute) @constant) (#eq? @constant "np\.pi") (#set! conceal "π"))
+(((attribute) @constant) (#match? @constant "^(math|np)\.pi$") (#set! conceal "π"))
 
 ;; Functions
 ;; used in function call contexts, e.g. `sum(...)` -> `∑(...)`
-((call function: (identifier) @function.builtin) (#eq? @function.builtin "sum") (#set! conceal "∑"))
 ((call function: (identifier) @function.builtin) (#eq? @function.builtin "all") (#set! conceal "∀"))
 ((call function: (identifier) @function.builtin) (#eq? @function.builtin "any") (#set! conceal "∃"))
-((call function: (identifier) @function.builtin) (#eq? @function.builtin "len") (#set! conceal "#"))
+((call function: (identifier) @function.builtin) (#eq? @function.builtin "product") (#set! conceal "∏"))
+((call function: (identifier) @function.builtin) (#eq? @function.builtin "sqrt") (#set! conceal "√"))
+((call function: (identifier) @function.builtin) (#eq? @function.builtin "sum") (#set! conceal "∑"))
 ((call function: (attribute) @method) (#eq? @method "math\.sqrt") (#set! conceal "√"))
 
 ;; Use a highlight group for test function/method definitions
@@ -21,7 +21,6 @@
           (function_definition
             name: (identifier) @function.method.test)))
  (#lua-match? @function.method.test "^test_"))
-
 ((function_definition
   name: (identifier) @function.test)
  (#lua-match? @function.test "^test_"))
