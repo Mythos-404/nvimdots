093cdb1b5c21d787caf34ed3e3ce9118653d9e58
Mythos_404 <mythos_404@outlook.com>
2025-06-15 04:37:21 +0800

    fix: remove manual `LspStart`

--- lua/modules/configs/completion/lsp.lua
+++ lua/modules/configs/completion/lsp.lua
@@ -1,13 +1,6 @@
 return function()
-    local nvim_lsp = require("lspconfig")
     require("completion.neoconf").setup()
     require("completion.mason").setup()
     require("completion.mason-lspconfig").setup()
     require("tool.dap.mason-dap").setup()
-
-    -- local opts = {
-    -- 	capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
-    -- }
-
-    pcall(vim.cmd.LspStart)
 end
